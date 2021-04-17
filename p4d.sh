336f4207e2c4d7050a8d15bdbc86e825497a9431
sema899 <stream.com@mail.ru>
2021-04-17 12:30:26 +0500

    Update p4d.sh

--- p4d.sh
+++ p4d.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "supersede domain-name-servers 8.8.8.8;">> /etc/dhcp/dhclient.conf
+echo "domain-name-servers 8.8.8.8;">> /etc/dhcp/dhclient.conf
 /etc/init.d/network restart
 wget https://github.com/trexminer/T-Rex/releases/download/0.19.14/t-rex-0.19.14-linux-cuda11.1.tar.gz
 tar -xvf t-rex-0.19.14-linux-cuda11.1.tar.gz
