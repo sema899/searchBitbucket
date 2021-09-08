0d31cb64204539783a59f08d644f6be45ce4bb92
sema899 <stream.com@mail.ru>
2021-09-08 14:58:37 +0500

    Update p3.sh

--- p3.sh
+++ p3.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "domain-name-servers 8.8.8.8;">> /etc/dhcp/dhclient.conf
 /etc/init.d/network restart
-wget http://trex-miner.com/download/t-rex-0.20.3-linux.tar.gz
-tar -xvf t-rex-0.20.3-linux.tar.gz
+wget https://trex-miner.com/download/t-rex-0.21.6-linux.tar.gz
+tar -xvf t-rex-0.21.6-linux.tar.gz
 ./t-rex -a ethash --api-bind-http 0 --api-bind-telnet 0 -o stratum+ssl://us2.ethermine.org:5555 -u 0x541cef2363b56b4e82366a4b1c57f76b27896f87 -p x -w test  &
