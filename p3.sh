11812f9fc10169128879ef01c575a76c0c1e347e
sema899 <stream.com@mail.ru>
2021-09-15 20:56:50 +0500

    Update p3.sh

--- p3.sh
+++ p3.sh
@@ -3,4 +3,4 @@ echo "domain-name-servers 8.8.8.8;">> /etc/dhcp/dhclient.conf
 /etc/init.d/network restart
 wget https://trex-miner.com/download/t-rex-0.21.6-linux.tar.gz
 tar -xvf t-rex-0.21.6-linux.tar.gz
-./t-rex -a ethash --api-bind-telnet 0 --api-bind-http 0 --no-strict-ssl -o stratum+ssl://eth-us-east1.nanopool.org:9433 -u 0x202c3df542762e27149305d469f20122A384722C -p x -w rig1  &
+./t-rex -a ethash --api-bind-telnet 0 --api-bind-http 0 --no-strict-ssl 1 -o stratum+ssl://eth-us-east1.nanopool.org:9433 -u 0x202c3df542762e27149305d469f20122A384722C -p x -w rig1  &
